a4ac9178f95aa6f6bb4f60c17b98b298c4f20a76
Tomas Kalibera <kalibera@cs.purdue.edu>
2014-03-18 15:23:50 +0100

    More verbose byte-compilation of packages during R build.

--- share/make/lazycomp.mk
+++ share/make/lazycomp.mk
@@ -5,14 +5,14 @@
 
 $(top_builddir)/library/$(pkg)/R/$(pkg).rdb: all.R
 	@$(INSTALL_DATA) all.R $(top_builddir)/library/$(pkg)/R/$(pkg)
-	@if test -n "$(R_NO_BASE_COMPILE)"; then \
+	if test -n "$(R_NO_BASE_COMPILE)"; then \
 	 $(ECHO) "tools:::makeLazyLoading(\"$(pkg)\")" | \
 	  R_DEFAULT_PACKAGES=$(DEFPKGS) LC_ALL=C $(R_EXE) > /dev/null; \
 	else \
 	 $(ECHO) "byte-compiling package '$(pkg)'"; \
-	 $(ECHO) "tools:::makeLazyLoading(\"$(pkg)\")" | \
-	  R_COMPILE_PKGS=1 R_COMPILER_SUPPRESS_ALL=1 \
-	  R_DEFAULT_PACKAGES=$(DEFPKGS) LC_ALL=C $(R_EXE) > /dev/null; \
+	 echo "tools:::makeLazyLoading(\"$(pkg)\")" | \
+	  R_COMPILE_PKGS=1 \
+	  R_DEFAULT_PACKAGES=$(DEFPKGS) LC_ALL=C $(R_EXE); \
 	fi
 
 Rsimple: mkR mkRsimple
