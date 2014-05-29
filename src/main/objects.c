dce893b5cddd41f6b04ca0ba15679a1a85efa248
Tomas Kalibera <kalibera@cs.purdue.edu>
2014-05-29 16:21:35 +0200

    Removed duplicate checkArity call from do_standardGeneric

--- src/main/objects.c
+++ src/main/objects.c
@@ -1171,7 +1171,7 @@ SEXP attribute_hidden do_standardGeneric(SEXP call, SEXP op, SEXP args, SEXP env
 {
     SEXP arg, value, fdef; R_stdGen_ptr_t ptr = R_get_standardGeneric_ptr();
 
-    checkArity(op, args);
+    checkArity(op, args); /* set to -1 */
     check1arg(args, call, "f");
 
     if(!ptr) {
@@ -1181,7 +1181,6 @@ SEXP attribute_hidden do_standardGeneric(SEXP call, SEXP op, SEXP args, SEXP env
 	ptr = R_get_standardGeneric_ptr();
     }
 
-    checkArity(op, args); /* set to -1 */
     arg = CAR(args);
     if(!isValidStringF(arg))
 	errorcall(call,
