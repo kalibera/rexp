9844c85c63052f42e87b3b5654abbd523eed9cd0
Tomas Kalibera <kalibera@cs.purdue.edu>
2015-12-11 11:25:04 +0100

    More robust parsing of R_COMPILE_PKGS.

--- src/library/tools/R/install.R
+++ src/library/tools/R/install.R
@@ -1084,7 +1084,6 @@
                 print(res[res$old > 1e5, ])
             }
 	}
-
 	if (install_tests && dir.exists("tests") &&
             length(dir("tests", all.files = TRUE) > 2L)) {
 	    starsmsg(stars, "tests")
@@ -1096,7 +1095,12 @@
             BC <- if (!is.na(byte_compile)) byte_compile
             else
                 parse_description_field(desc, "ByteCompile", default = FALSE)
-            rcp <- as.numeric(Sys.getenv("R_COMPILE_PKGS"))
+            rcps <- Sys.getenv("R_COMPILE_PKGS")
+            rcp <- switch(rcps,
+                "TRUE"=, "true"=, "True"=, "yes"=, "Yes"= 1,
+                "FALSE"=,"false"=,"False"=, "no"=, "No" = 0,
+                as.numeric(rcps)
+            )
             BC <- BC || (!is.na(rcp) && rcp > 0)
             if (BC) {
                 starsmsg(stars,
