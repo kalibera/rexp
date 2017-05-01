96d325e172dd72fb00386395931cacda61f5eae4
Tomas Kalibera <kalibera@cs.purdue.edu>
2017-05-01 22:55:46 +0200

    Do not repromise missing arg.

--- src/main/eval.c
+++ src/main/eval.c
@@ -2789,7 +2789,10 @@ SEXP attribute_hidden promiseArgs(SEXP el, SEXP rho)
 	    PROTECT(h = findVar(CAR(el), rho));
 	    if (TYPEOF(h) == DOTSXP || h == R_NilValue) {
 		while (h != R_NilValue) {
-		    SETCDR(tail, CONS(mkPROMISE(CAR(h), rho), R_NilValue));
+		    if (CAR(h) == R_MissingArg)
+		      SETCDR(tail, CONS(R_MissingArg, R_NilValue));
+                    else
+		      SETCDR(tail, CONS(mkPROMISE(CAR(h), rho), R_NilValue));
 		    tail = CDR(tail);
 		    COPY_TAG(tail, h);
 		    h = CDR(h);
