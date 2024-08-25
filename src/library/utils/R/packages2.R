ee290e55f86a9b8f5adb53db5a804c4588980aed
Tomas Kalibera <tomas.kalibera@gmail.com>
2024-08-25 10:55:03 +0200

    Imported GNU-R version 87049.

--- src/library/utils/R/packages2.R
+++ src/library/utils/R/packages2.R
@@ -756,7 +756,8 @@ install.packages <-
     av2 <- NULL
     if(is.null(available)) {
         filters <- getOption("available_packages_filters")
-        if(!is.null(filters)) {
+        if(!is.null(filters) ||
+           any(!is.na(pmatch(...names(), "filters")))) {
             available <- available.packages(contriburl = contriburl,
                                             method = method, ...)
         } else {
--- tests/reg-tests-3.R
+++ tests/reg-tests-3.R
@@ -201,16 +201,9 @@ nchar(x, "w", allowNA = TRUE)
 
 
 ## str() on large strings
-if (l10n_info()$"UTF-8" || l10n_info()$"Latin-1") {
-  cc <- "J\xf6reskog" # valid in "latin-1"; invalid multibyte string in UTF-8
-  .tmp <- capture.output(
-  str(cc) # failed in some R-devel versions
-  )
-  stopifnot(grepl("chr \"J.*reskog\"", .tmp))
+nchar(L <- strrep(paste(LETTERS, collapse="."), 100000), type="b") # 5.1 M
+str(L)
 
-  print(nchar(L <- strrep(paste(LETTERS, collapse="."), 100000), type="b")) # 5.1 M
-  print(str(L))
-}
 
 if(require("Matrix", .Library)) {
     M <- Matrix(diag(1:10), sparse=TRUE) # a "ddiMatrix"
@@ -253,7 +246,7 @@ stopifnot(exprs = {
 pkg <- "nlme"
 (hasME <- requireNamespace(pkg, quietly=TRUE, lib.loc = .Library))
 if(hasME) withAutoprint({
-    c2 <- citation(package=pkg)
+    c2 <- citation(pkg, .Library)
     ## avoid spurious diffs:
     c2$author[[1]]$given[[1]] <- "J."
     c2$year[[1]] <- "9999"
@@ -277,6 +270,13 @@ if(hasME) withAutoprint({
         nchar(f2N[ie]) < nchar(f2B[ie])
         startsWith(f2B[ie], f2N[ie])
       })
+
+    desc <- packageDescription(pkg, .Library)
+    desc$URL <- paste(URL1 <- "https://example.org",
+                      "https://example.com", sep = "\n") # via continuation line
+    desc$Repository <- NULL
+    c3 <- citation(auto = desc)
+    stopifnot(identical(print(c3$url), URL1)) # R <= 4.4.0 gave both URLs
 })
 
 cat('Time elapsed: ', proc.time(),'\n')
